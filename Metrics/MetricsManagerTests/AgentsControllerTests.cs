266887762bc8f2e8b7c72b1381d97d7ccb6ad4ad
Tanya <yallina.ta@yandex.ru>
2022-05-07 20:55:39 +1000

    Lesson2
    Lesson2

---
+++ Metrics/MetricsManagerTests/AgentsControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using MetricsManager.Controllers;
+using MetricsManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MetricsManagerTests
+{
+    public class AgentsControllerTests
+    {
+        private AgentsController _agentsController;
+        private AgentPool _agentPool;
+
+        public AgentsControllerTests()
+        {
+            _agentPool = new AgentPool();
+            _agentsController = new AgentsController(_agentPool);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void RegisterAgentTest(int agentId)
+        {
+            AgentInfo agentInfo = new AgentInfo() { AgentId = agentId, Enable = true };
+            IActionResult actionResult = _agentsController.RegisterAgent(agentInfo);
+            Assert.IsAssignableFrom<IActionResult>(actionResult);
+        }
+
+        [Fact]
+        public void GetAgentsTest()
+        {
+            IActionResult actionResult = _agentsController.GetAllAgents();
+            OkObjectResult result = Assert.IsAssignableFrom<OkObjectResult>(actionResult);
+            Assert.NotNull(result.Value as IEnumerable<AgentInfo>);
+        }
+    }
+}
